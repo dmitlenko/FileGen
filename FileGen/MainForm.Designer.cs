45bcdabfa69cfb386e3283c5100d789128cf758a
nothing <softonka@gmail.com>
2020-11-20 18:31:50 +0200

    Fully working prototype update

--- FileGen/MainForm.Designer.cs
+++ FileGen/MainForm.Designer.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace FileGen
 {
-    partial class Form1
+    partial class MainForm
     {
         /// <summary>
         /// Обязательная переменная конструктора.
@@ -29,13 +29,146 @@ namespace FileGen
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.fileCreator = new System.Windows.Forms.SaveFileDialog();
+            this.filenameField = new System.Windows.Forms.TextBox();
+            this.createButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.filesizeField = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.link = new System.Windows.Forms.LinkLabel();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // fileCreator
+            // 
+            this.fileCreator.FileOk += new System.ComponentModel.CancelEventHandler(this.fileCreator_FileOk);
+            // 
+            // filenameField
+            // 
+            this.filenameField.Location = new System.Drawing.Point(8, 72);
+            this.filenameField.Name = "filenameField";
+            this.filenameField.Size = new System.Drawing.Size(256, 20);
+            this.filenameField.TabIndex = 0;
+            // 
+            // createButton
+            // 
+            this.createButton.Location = new System.Drawing.Point(200, 152);
+            this.createButton.Name = "createButton";
+            this.createButton.Size = new System.Drawing.Size(64, 24);
+            this.createButton.TabIndex = 1;
+            this.createButton.Text = "Create";
+            this.createButton.UseVisualStyleBackColor = true;
+            this.createButton.Click += new System.EventHandler(this.createButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(8, 56);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "File name:";
+            // 
+            // filesizeField
+            // 
+            this.filesizeField.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.filesizeField.FormattingEnabled = true;
+            this.filesizeField.Items.AddRange(new object[] {
+            "1 Mb",
+            "5 Mb",
+            "10 Mb",
+            "50 Mb",
+            "100 Mb",
+            "500 Mb",
+            "1000 Mb",
+            "2000 Mb",
+            "3000 Mb",
+            "4000 Mb",
+            "5000 Mb"});
+            this.filesizeField.Location = new System.Drawing.Point(8, 120);
+            this.filesizeField.Name = "filesizeField";
+            this.filesizeField.Size = new System.Drawing.Size(256, 21);
+            this.filesizeField.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(8, 104);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "File size:";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.InitialImage = null;
+            this.pictureBox1.Location = new System.Drawing.Point(8, 8);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(256, 40);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // link
+            // 
+            this.link.AutoSize = true;
+            this.link.Location = new System.Drawing.Point(8, 184);
+            this.link.Name = "link";
+            this.link.Size = new System.Drawing.Size(66, 13);
+            this.link.TabIndex = 6;
+            this.link.TabStop = true;
+            this.link.Text = "by dmitlenko";
+            this.link.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.link_LinkClicked);
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(208, 184);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "v1.0";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(270, 208);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.link);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.filesizeField);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.createButton);
+            this.Controls.Add(this.filenameField);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.Text = "FileGen";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.SaveFileDialog fileCreator;
+        private System.Windows.Forms.TextBox filenameField;
+        private System.Windows.Forms.Button createButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox filesizeField;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.LinkLabel link;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- FileGen/MainForm.cs
+++ FileGen/MainForm.cs
@@ -3,18 +3,50 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace FileGen
 {
-    public partial class Form1 : Form
+    public partial class MainForm : Form
     {
-        public Form1()
+        private void createFile(string path,int size)
+        {
+            FileStream fs = new FileStream(@path, FileMode.OpenOrCreate);
+            fs.Seek(((long)size) * 1024 * 1024, SeekOrigin.Begin);
+            fs.WriteByte(0);
+            fs.Close();
+        }
+        public MainForm()
         {
             InitializeComponent();
+        }
+        
+        private void link_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://github.com/dmitlenko/FileGen");
+        }
+
+        private void createButton_Click(object sender, EventArgs e)
+        {
+            fileCreator.FileName = filenameField.Text;
+            fileCreator.ShowDialog();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            filesizeField.SelectedIndex = 0;
+            filenameField.Text = "myfile.bin";
+        }
+
+        private void fileCreator_FileOk(object sender, CancelEventArgs e)
+        {
+            string input = filesizeField.SelectedItem.ToString();
+            createFile(fileCreator.FileName, Convert.ToInt32(Regex.Replace(input, "[^0-9]", "")));
         }
     }
 }
